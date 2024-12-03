0ca4da9481d61274e6d131d2ec39153e6dda0a99
khaled Nasr <kn3846228@gmail.com>
2024-12-03 21:49:06 +0200

    Add files via upload

---
+++ GdG Task/main.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector('.header .nav-bar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.add('active');
+}
+
+document.querySelector('#close-navbar').onclick = () =>{
+    navbar.classList.remove('active');
+}
